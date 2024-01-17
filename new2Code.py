31bef966242c7163a3e32c5688f3bd36baac7e28
shubhalakshmi B S <shubhalakshmibs@nousinfo.com>
2024-01-17 12:59:07 +0530

    update

--- new2Code.py
+++ new2Code.py
@@ -6,3 +6,12 @@ os.rmdir("local_path")
 # import shutil
 # # shutil.rmtree(folder_path) 
 print("Temporary directory deleted.")
+
+
+# os.system('rm -rf local_path')
+
+# # #deleting created folder
+# os.rmdir(local_path)
+# # import shutil
+# # # shutil.rmtree(folder_path) 
+# print("Temporary directory deleted.")
