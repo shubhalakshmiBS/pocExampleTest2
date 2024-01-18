beda2527df8a6ca0be186047a7c51827260acf95
shubhalakshmi B S <shubhalakshmibs@nousinfo.com>
2024-01-18 17:05:40 +0530

    newthings

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,1 +1,16 @@
 print("asdfghj")
+
+
+def compare_and_fetch_diff(file_path):
+    prev_blob = ""
+    prev_content = prev_blob.data_stream.read().decode("utf-8")
+ 
+    current_blob = ""
+    current_content = current_blob.data_stream.read().decode("utf-8")
+ 
+    diff = ""
+    changes = [line for line in diff if line.startswith(("+", "-"))]
+
+    print("Change in this {} file---------------------------------".format(file_path))
+    print(changes)
+    return changes
--- new2Code.py
+++ new2Code.py
@@ -8,14 +8,3 @@ os.rmdir("local_path")
 print("Temporary directory deleted.")
 
 
-# os.system('rm -rf local_path')
-
-# # #deleting created folder
-# os.rmdir(local_path)
-# # import shutil
-# # # shutil.rmtree(folder_path) 
-# print("Temporary directory deleted.")
-
-
-print("Hello TEAM")
-print("am printing changes")
